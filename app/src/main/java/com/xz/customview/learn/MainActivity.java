2d3b2f4a4e75cd87c532239ca3d01a74faec429a
zhoulei <zhoulei@xiaozhu.com>
2019-11-06 10:40:07 +0800

    dfsafsdafasdfsadfsadf adflasjflasfjcdslafj11:wq :wq

--- app/src/main/java/com/xz/customview/learn/MainActivity.java
+++ app/src/main/java/com/xz/customview/learn/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //测试commit
+        //测试commit1
         String s = "111";
     }
 }
